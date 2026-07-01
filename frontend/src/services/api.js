a3146c5a75bbe57ac8a2048ec14908601d3c5fc0
Rakshitha H R <rakshitha.hr@campusuvce.in>
2026-07-01 22:30:07 +0530

    p

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Flask Backend Base URL
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://ocr-document-processing-system.onrender.com",
   headers: {
     "Content-Type": "multipart/form-data",
   },
